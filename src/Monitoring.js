90c68d442e2329af76cf60ca42723981a4c50a01
Thomas Ederer <thomas@orchideenvermehrung.at>
2024-02-28 18:47:38 +0000

    fixed issue with monitoring data

--- src/Monitoring.js
+++ src/Monitoring.js
@@ -12,34 +12,25 @@ octowatch.Monitoring = function Monitoring() {
    var TIMEOUT_IN_MS = 10000;
    var LOGGER = common.logging.LoggingSystem.createLogger('Monitoring');
    
-   var currentValues       = {};
-   var nextCurrentValuesId = 1;
-   
-   var nextId = function nextId() {
-      var idToReturn      = nextCurrentValuesId;
-      nextCurrentValuesId = (nextCurrentValuesId + 1) % 1000;
-      return idToReturn;
-   };
+   var currentValues = {};
    
    var removeTooOldValues = function removeTooOldValues() {
       var nowInMs =  Date.now();
       
-      if (currentValues.temperatures !== undefined) {
-         Object.keys(currentValues.temperatures).forEach(key => {
-            var age = nowInMs - currentValues.temperatures[key].timestamp;
-            if (age >= TIMEOUT_IN_MS) {
-               LOGGER.logDebug('temperature ' + key + ' timed out -> removing it');
-               currentValues.temperatures[key] = undefined;
-            }
-         });
-      }
-      
-      if (currentValues.humidity !== undefined) {
-         var age = nowInMs - currentValues.humidity.timestamp;
+      Object.keys(currentValues.temperatures ?? {}).forEach(key => {
+         var temperature = currentValues.temperatures[key] ?? {};
+         var age         = nowInMs - (temperature.timestamp ?? 0);
          if (age >= TIMEOUT_IN_MS) {
-            LOGGER.logDebug('humidity timed out -> removing it');
-            currentValues.humidity = undefined;
+            LOGGER.logDebug('temperature ' + key + ' timed out -> removing it');
+            currentValues.temperatures[key] = undefined;
          }
+      });
+      
+      var humidity = currentValues.humidity ?? {};
+      var age      = nowInMs - (humidity.timestamp ?? 0);
+      if (age >= TIMEOUT_IN_MS) {
+         LOGGER.logDebug('humidity timed out -> removing it');
+         currentValues.humidity = undefined;
       }
       
       setTimeout(removeTooOldValues, 1000);
@@ -47,13 +38,11 @@ octowatch.Monitoring = function Monitoring() {
    
    var onHumidityUpdate = function onHumidityUpdate(newHumidity) {
       LOGGER.logDebug('new humidity: ' + newHumidity);
-      currentValues.id = nextId();
       currentValues.humidity = {value: newHumidity, timestamp: Date.now()};
    };
    
    var onTemperatureUpdate = function onTemperatureUpdate(key, temperature) {
       LOGGER.logDebug('new ' + key + ' temp: ' + temperature);
-      currentValues.id = nextId();
       if (currentValues.temperatures === undefined) {
          currentValues.temperatures = {};
       }
